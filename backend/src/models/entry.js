29317b7ce2adf4ff7b7eefa76341e7b7f3819d23
Pawan Kolhe <contact@pawankolhe.com>
2021-03-29 12:18:37 +0530

    feat: update entry model

--- backend/src/models/entry.js
+++ backend/src/models/entry.js
@@ -4,6 +4,51 @@ import { composeWithMongoose } from 'graphql-compose-mongoose';
 
 export const EntrySchema = new Schema(
   {
+    entry_id: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    state_patient_number: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    date_announced: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    age_bracket: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    gender: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    detected_city: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    detected_district: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    detected_state: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    state_code: {
+      type: String,
+      trim: true,
+      required: false,
+    },
     num_cases: {
       type: Number,
       trim: true,
@@ -14,7 +59,32 @@ export const EntrySchema = new Schema(
       trim: true,
       required: false,
     },
-    date_announced: {
+    contracted_patient: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    notes: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    source_1: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    nationality: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    type_of_transmission: {
+      type: String,
+      trim: true,
+      required: false,
+    },
+    patient_number: {
       type: String,
       trim: true,
       required: false,
